5819c648e4cb94989665837e049fc4754dae924f
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2020-06-27 12:34:00 +0300

    Example Api

---
+++ DistributedApi/Controllers/DateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DistributedApi.Extension;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistributedApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DateController : ControllerBase
+    {
+        [HttpGet("Get")]
+        public IActionResult Get() => Ok(DateTime.Now.LocalizeTimeStamp());
+
+        [HttpGet("Custom")]
+        public IActionResult Custom(int year, int month, int day) => Ok(new DateTime(year, month, day).LocalizeTimeStamp());
+    }
+}
---
+++ DistributedApi/Extension/Extension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace DistributedApi.Extension
+{
+    public static class Extension
+    {
+        // 1 Jan 1970 is begin of UNIX Time
+        public static int LocalizeTimeStamp(this DateTime value) => (Int32)(value.ToUniversalTime().Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+    }
+}
